c7d9d3a000d88127c4ff47f3660b75effb0058e4
artir <artir@REYANSH2021>
2023-03-07 11:31:04 +0530

    Add project files.

---
+++ FunctionWithAndWithoutReturnType/Doctor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionWithAndWithoutReturnType
+{
+    internal class Doctor
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GrossIncome { get; set; }
+        public int IncomeTax { get; set; }
+        public int NetIncome { get; set; }
+
+        //non value returning  function 
+        public void GetData()
+        {
+            Console.WriteLine("Enter any value for Id");
+            Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value fro Name");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter the value for GrossIncome");
+            GrossIncome = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the value for IncomeTax");
+            IncomeTax = Convert.ToInt32(Console.ReadLine());
+
+            NetIncome = CalculateNetIncome();
+
+
+        }
+
+        //non value function without return type
+        public void PutData()
+        {
+            Console.WriteLine("Id="+Id);
+            Console.WriteLine("Name="+Name);
+            Console.WriteLine("GrossIncome="+GrossIncome);
+            Console.WriteLine("IncomeTax"+IncomeTax);
+            Console.WriteLine("NetIncome="+NetIncome);
+        }
+
+        //value returning method
+        public int CalculateNetIncome()
+        {
+            int result = GrossIncome - IncomeTax;
+            return result;
+        }
+    }
+}
---
+++ FunctionWithAndWithoutReturnType/Program.cs
@@ -0,0 +1,18 @@
+﻿using FunctionWithAndWithoutReturnType;
+
+public class  program
+{
+    public static void Main()
+    {
+        Doctor doctor1 = new Doctor();
+        Console.WriteLine("please enter doctor1 object");
+        doctor1.GetData();
+        doctor1.PutData();
+
+        Doctor doctor2 = new Doctor();
+        Console.WriteLine("please enter doctor2 object");
+        doctor2.GetData();
+        doctor2.PutData();
+    }
+
+}
