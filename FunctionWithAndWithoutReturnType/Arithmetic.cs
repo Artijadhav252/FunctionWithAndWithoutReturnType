2a62cf94f7b55f2db13fbf7bacaf1beb865fb1e1
artir <artir@REYANSH2021>
2023-03-08 10:36:57 +0530

    Input Output Parameters

---
+++ FunctionWithAndWithoutReturnType/Arithmetic.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionWithAndWithoutReturnType
+{
+    internal class Arithmetic
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        //parameters or arguments
+        public void add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine($"addition of{num1}and {num2} is = {result}");
+        }
+        //public void sub(int num1,int num2)
+        //{
+        //    int result = num1 - num2;
+        //    Console.WriteLine($"Substraction of {num1} and {num2} is={result}");
+        //}
+        //public void mul(int num1, int num2)
+        //{
+        //    int result = num1 * num2;
+        //    Console.WriteLine($"Multiplication of {num1} and {num2} is ={result}");
+        //}
+        //public void div(int num1, int num2) 
+        //{
+        //     int result = num1 / num2;
+        //    Console.WriteLine($"Division of {num1} AND {num2} is = {result}");
+        //}
+
+        //Complex argument or parameters
+        public void add(Arithmetic arth)
+        {
+            int result = arth.A + arth.B;
+            Console.WriteLine($"Assition of{arth.A} Ans {arth.B} IS = {result}");
+        }
+
+        //public void sub(Arithmetic arth)
+        //{
+        //    int result=arth.A- arth.B;
+        //    Console.WriteLine($"Substraction of {arth.A} and {arth.B} is = {result}");
+        //}
+
+        //public void mul(Arithmetic arth)
+        //{
+        //    int result = arth.A * arth.B;
+        //    Console.WriteLine($"Multiplication of {arth.A} and {arth.B} is= {result}");
+        //}
+
+        //public void div(Arithmetic arth)
+        //{
+        //    int result = arth.A / arth.B;
+        //    Console.WriteLine($"Division of {arth.A} And {arth.B} is = {result}");
+        //}
+
+        //output parameter
+        public void Add(int num1,int num2,out int result)
+        {
+            result = num1 + num2;
+        }
+       
+
+    }   
+
+}
--- FunctionWithAndWithoutReturnType/Program.cs
+++ FunctionWithAndWithoutReturnType/Program.cs
@@ -1,20 +1,56 @@
 ﻿using FunctionWithAndWithoutReturnType;
 
-public class  program
+public class  Program
 {
     public static void Main()
     {
-        Doctor doctor1 = new Doctor();
-        Console.WriteLine("please enter doctor1 object");
-        doctor1.GetData();
-        doctor1.PutData();
+        Arithmetic arithmetic = new Arithmetic();
+        arithmetic.add(10, 20);
 
-        Doctor doctor2 = new Doctor();
-        Console.WriteLine("please enter doctor2 object");
-        doctor2.GetData();
-        doctor2.PutData();
+        //Arithmetic arithmetic1 = new Arithmetic();  
+        //arithmetic1.sub(30, 20);
 
-        Doctor.typecasting();
+        //Arithmetic arithmetic2 = new Arithmetic();
+        //arithmetic2.mul(3, 2);
+
+
+        //Arithmetic arithmetic3 = new Arithmetic();
+        //arithmetic3.div(20, 2);
+
+
+        //Arithmetic arithmetic1 = new Arithmetic();
+        //arithmetic1.A = 40;
+        //arithmetic1.B = 20;
+
+        //Arithmetic arithmetic2 =  new Arithmetic();
+        //arithmetic2.add(arithmetic1);
+
+        //Arithmetic arithmetic3=new Arithmetic();    
+        //arithmetic3.sub(arithmetic1);
+
+        //Arithmetic arithmetic4 = new Arithmetic();
+        //arithmetic4.mul(arithmetic1);
+
+        //Arithmetic arithmetic5= new Arithmetic();
+        //arithmetic5.div(arithmetic1);
+
+        arithmetic.add(10, 20);
+        int result;
+        arithmetic.Add(10, 20,out result);
+        Console.WriteLine($"Multiplication of 10 and 20 is ={result}");
+
+
+        //Doctor doctor1 = new Doctor();
+        //Console.WriteLine("please enter doctor1 object");
+        //doctor1.GetData();
+        //doctor1.PutData();
+
+        //Doctor doctor2 = new Doctor();
+        //Console.WriteLine("please enter doctor2 object");
+        //doctor2.GetData();
+        //doctor2.PutData();
+
+        //Doctor.typecasting();
     }
 
 }
